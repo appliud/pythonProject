4d86b09c3ab1999d45ed8043b654acb541f1cfca
wrr <3615336590@qq.com>
2025-03-13 21:50:46 +0800

    1

--- Users/xxx/tf/dome/tf_enumerate.py
+++
@@ -1,4 +0,0 @@
-# enumerate遍历带有索引的序列
-seq=['one', 'two', 'three', 'four', 'five']
-for i, el in enumerate(seq):
-    print(i, el)
--- Users/xxx/tf/dome/tf_nn_softmax.py
+++
@@ -1,7 +0,0 @@
-# tf.nn.softmax() 使输出符合概率分布
-# nn神经网络
-import tensorflow as tf
-
-x = tf.constant([[1., 2., 3.], [4., 5., 6.]])
-print(x)
-print(tf.nn.softmax(x))
